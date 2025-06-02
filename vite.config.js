32d69ab7ad89f9ce8ac4ecc95cec5cc0e253835f
thinkand-port <your@email.com>
2025-06-02 19:10:53 +0530

    fixed vercel error

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,36 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   build: {
     outDir: "dist",
-    chunkSizeWarningLimit: 1000, // Increase chunk size limit to avoid warnings
+    chunkSizeWarningLimit: 1500, // Optional: raised to handle larger chunks
   },
   plugins: [
     react(),
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
-        src: 'manifest.json',
+        name: "Wani",
+        short_name: "SpeedApp",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#0ea5e9",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // ✅ Set to 5MB
       },
     }),
   ],
